7bf4d738b4253bb0741c4e6bbedd25020aef7f83
Luan Rodrigues Paz <luropa_paz@hotmail.com>
2022-09-23 04:18:38 -0300

    version 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Microsoft Adaptive Cards'
 LONG_DESCRIPTION = 'A package that helps you create adaptive cards.'
 
