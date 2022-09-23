636a527075257c92aaa4183e7a51ab3368d5200b
Luan Rodrigues Paz <luropa_paz@hotmail.com>
2022-09-23 03:33:39 -0300

    Initial commit

--- entities/content.py
+++
@@ -1,7 +0,0 @@
-from entities.adaptivecard import AdaptiveCard
-
-class Content:
-    """Content é o elemento que recebe o AdaptiveCard e é adicionado à lista atachments, atributo de Message"""
-    def __init__(self, card: AdaptiveCard):
-        self.contentType = "application/vnd.microsoft.card.adaptive"
-        self.content = card.__dict__
