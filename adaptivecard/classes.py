63c484949784fb33981fb28199acc759c4e1e610
Luan Paz <luropa_paz@hotmail.com>
2023-06-16 04:54:21 -0300

    Cria novo type checking e protege atributos

--- adaptivecard/classes.py
+++ adaptivecard/classes.py
@@ -1,5 +1,9 @@
-from adaptivecard.mixin import Mixin
-from typing import Union, List
+from mixin import Mixin
+from typing import Union, List, Optional, Any
+
+
+class CardElement():
+    pass
 
 
 class Content:
@@ -11,7 +15,7 @@ class Content:
 
 class Message(Mixin):
     """"Estrutura final do card tal como se requer para envio a um canal do Teams"""
-    def __init__(self, attachments: Union[None, List["Content"]] = None):
+    def __init__(self, attachments: Optional[List["Content"]] = None):
         self.type = "message"
         if attachments is None:
             attachments = []
@@ -25,21 +29,32 @@ class Message(Mixin):
 
 class AdaptiveCard(Mixin):
     """O template principal do card"""  # Essas descrições hão de ficar mais detalhadas à medida que eu desenvolver a lib e sua documentação
-    def __init__(self, type: str = "AdaptiveCard", version="1.2", schema: str = "http://adaptivecards.io/schemas/adaptive-card.json",
-                 body: Union[None, List[Union["ColumnSet", "Container", "TextBlock", "Image"]]] = None,
-                 falbackText: Union[None, str] = None, backgroundImage=None, minHeight=None, rtl=None,
-                 speak=None, lang=None, verticalContentAlignment=None):
-        self.type = type
-        self.version = version
-        super().set_version(self.version)
-        self.schema = schema
+    def __init__(self, version="1.2",
+                 schema: str = "http://adaptivecards.io/schemas/adaptive-card.json",
+                 body: Optional[List[Union["ColumnSet", "Container", "TextBlock", "Image"]]] = None,
+                 fallbackText: Optional[str] = None,
+                 backgroundImage: Optional[str] = None,
+                 minHeight: Optional[str] = None,
+                 rtl: Optional[bool] = None,
+                 speak: Optional[str] = None,
+                 lang: Optional[str] = None,
+                 verticalContentAlignment: Optional[str] = None):
 
         if body is None:
             body = []
 
+        self.type = "AdaptiveCard"
+        self.version = version
+        self.schema = schema
         self.body = body
-        # atributo empty!!
-        super().create_fields(locals())
+        self.fallbackText = fallbackText
+        self.backgroundImage = backgroundImage
+        self.minHeight = minHeight
+        self.rtl = rtl
+        self.speak = speak
+        self.lang = lang
+        self.verticalContentAlignment = verticalContentAlignment
+        self.json_fields = frozenset(self.get_local_vars())
 
     @property
     def empty(self):
@@ -48,20 +63,47 @@ class AdaptiveCard(Mixin):
     def add_to_body(self, card_element):
         self.body.append(card_element)
 
+    def to_message(self):
+        content = Content(card=self)
+        msg = Message(attachments=[content])
+        return msg
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == 'verticalContentAlignment' and __value not in (allowed_values := ("top", "center", "bottom")):
+            raise AttributeError(f"'{__name}' atribute can only take either of the following values: {', '.join(allowed_values)}")
+        return super().__setattr__(__name, __value)
+
 # ---------------------Containers------------------------- #
 
 
 class Container(Mixin):
     """Um contâiner é um agrupamento de elementos"""
-    def __init__(self, items=None, style: Union[None, str] = None, verticalContentAlignment: Union[None, str] = None,
-                 bleed: Union[None, bool] = None, minHeight: Union[None, str] = None, rtl: Union[None, bool] = None):
-        self.type = "Container"
+    def __init__(self,
+                 items: Optional[list] = None,
+                 style: Optional[str] = None,
+                 verticalContentAlignment: Optional[str] = None,
+                 bleed: Optional[bool] = None,
+                 minHeight: Optional[str] = None,
+                 rtl: Optional[bool] = None,
+                 height: Optional[str] = None,
+                 separator: Optional[str] = None,
+                 id: Optional[str] = None,
+                 isVisible: Optional[bool] = None):
 
         if items is None:
             items = []
-        self.items = items
 
-        super().create_fields(locals())
+        self.type = "Container"
+        self.items = items
+        self.style = style
+        self.verticalContentAlignment = verticalContentAlignment
+        self.bleed = bleed
+        self.minHeight = minHeight
+        self.rtl = rtl
+        self.height = height
+        self.separator = separator
+        self.id = id
+        self.isVisible = isVisible
 
     @property
     def empty(self):
@@ -70,81 +112,190 @@ class Container(Mixin):
     def add_to_items(self, card_element):
         self.items.append(card_element)
 
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == 'style' and __value not in (allowed_values := ("default", "emphasis", "good", "attention", "warning", "accent")):
+            raise AttributeError(f"'{__name}' attribute can only take either of the following values: {', '.join(allowed_values)}")
+        if __name == 'verticalContentAlignment' and __value not in (allowed_values := ("top", "center", "bottom")):
+            raise AttributeError(f"'{__name}' atribute can only take either of the following values: {', '.join(allowed_values)}")
+        if __name == 'height' and __value not in (allowed_values := ("auto", "stretch")):
+            raise AttributeError(f"'{__name}' atribute can only take either of the following values: {', '.join(allowed_values)}")
+        if __name == 'spacing' and __value not in (allowed_values := ("default", "none", "small", "medium", "large", "extraLarge", "padding")):
+            raise AttributeError(f"'{__name}' atribute can only take either of the following values: {', '.join(allowed_values)}")
+        return super().__setattr__(__name, __value)
 
-class ColumnSet(Mixin):
+
+class ColumnSet(Mixin, CardElement):
     """ColumnSet define um grupo de colunas"""
-    def __init__(self, columns: Union[None, List["Column"]] = None, style: Union[None, str] = None, bleed: Union[None, bool] = None,
-                 minHeight: Union[None, str] = None, horizontalAlignment: Union[None, str] = None, height: Union[None, str] = None,
-                 separator: Union[None, bool] = None, spacing: Union[None, str] = None, id: Union[None, str] = None,
-                 isVisible: Union[None, bool] = None):
-        self.type = "ColumnSet"
+    def __init__(self, columns: Optional[List["Column"]] = None,
+                 style: Optional[str] = None,
+                 bleed: Optional[bool] = None,
+                 minHeight: Optional[str] = None,
+                 horizontalAlignment: Optional[str] = None,
+                 height: Optional[str] = None,
+                 separator: Optional[bool] = None,
+                 spacing: Optional[str] = None,
+                 id_: Optional[str] = None,
+                 isVisible: Optional[bool] = None):
+
         if columns is None:
             columns = []
-        self.columns = columns
 
-        super().create_fields(locals())
+        self.type = 'ColumnSet'
+        self.columns = columns
+        self.style = style
+        self.bleed = bleed
+        self.minHeight = minHeight
+        self.horizontalAlignment = horizontalAlignment
+        self.height = height
+        self.separator = separator
+        self.spacing = spacing
+        self.id = id_
+        self.isVisible = isVisible
 
     def add_to_columns(self, column_element):
         self.columns.append(column_element)
 
 
-class Column(Mixin):
+class Column(Mixin, CardElement):
     """O contâiner Column define um elemento de coluna, que é parte de um ColumnSet."""
-    def __init__(self, items: Union[None, List[Union["Image", "TextBlock"]]] = None, backgroundImage=None,
-                 bleed: Union[None, bool] = None, fallback=None, minHeight: Union[None, str] = None,
-                 rtl: Union[None, bool] = None, separator: Union[None, bool] = None,
-                 spacing: Union[None, str, int] = None, style: Union[None, str] = None, verticalContentAlignment: Union[None, str] = None,
-                 width: Union[None, str, int] = None, id: Union[None, str] = None, isVisible: Union[None, bool] = None):
-        self.type = "Column"
+    def __init__(self,
+                 items: Optional[List[Union["Image", "TextBlock"]]] = None,
+                 backgroundImage=None,
+                 bleed: Optional[bool] = None,
+                 fallback: Optional["Column"] = None,
+                 minHeight: Optional[str] = None,
+                 rtl: Optional[bool] = None,
+                 separator: Optional[bool] = None,
+                 spacing: Optional[Union[str, int]] = None,
+                 style: Optional[str] = None,
+                 verticalContentAlignment: Optional[str] = None,
+                 width: Optional[Union[str, int]] = None,
+                 id_: Optional[str] = None,
+                 isVisible: Optional[bool] = None):
+
         if items is None:
             items = []
 
-        super().create_fields(locals())
+        self.type = "Column"
+        self.items = items
+        self.backgroundImage = backgroundImage
+        self.bleed = bleed
+        self.fallback = fallback
+        self.minHeight = minHeight
+        self.rtl = rtl
+        self.separator = separator
+        self.spacing = spacing
+        self.style = style
+        self.verticalContentAlignment = verticalContentAlignment
+        self.width = width
+        self.id = id_
+        self.isVisible = isVisible
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        return super().__setattr__(__name, __value)
 
     def add_to_items(self, card_element):
         self.items.append(card_element)
 
 
-class Table(Mixin):
-    def __init__(self, columns: Union[None, List[dict]], rows: Union[None, List[dict]],
-                 firstRowAsHeader: Union[None, bool], showGridLines: Union[None, bool],
-                 gridStyle: Union[None, str], horizontalCellContentAlignment: Union[None, str] = None,
-                 verticalCellContentAlignment: Union[None, str] = None,
-                 fallback: Union[None, "ColumnSet", "Container", "Image", "Table"] = None, height: Union[None, str] = None,
-                 separator: Union[None, bool] = None, spacing: Union[None, str] = None, id: Union[None, str] = None,
-                 isVisible: Union[None, bool] = None):
+class Table(Mixin, CardElement):
+    def __init__(self, columns: Optional[List[dict]], rows: Optional[List[dict]],
+                 firstRowAsHeader: Optional[bool], showGridLines: Optional[bool],
+                 gridStyle: Optional[str], horizontalCellContentAlignment: Optional[str] = None,
+                 verticalCellContentAlignment: Optional[str] = None,
+                 fallback: Optional[Union["ColumnSet", "Container", "Image", "Table"]] = None,
+                 height: Optional[str] = None,
+                 separator: Optional[bool] = None,
+                 spacing: Optional[str] = None,
+                 id_: Optional[str] = None,
+                 isVisible: Optional[bool] = None):
+
         self.type = "Table"
+        self.columns = columns
+        self.rows = rows
+        self.firstRowAsHeader = firstRowAsHeader
+        self.showGridLines = showGridLines
+        self.gridStyle = gridStyle
+        self.horizontalCellContentAlignment = horizontalCellContentAlignment
+        self.verticalCellContentAlignment = verticalCellContentAlignment
+        self.fallback = fallback
+        self.height = height
+        self.separator = separator
+        self.spacing = spacing
+        self.id = id_
+        self.isVisible = isVisible
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        return super().__setattr__(__name, __value)
 
 
 class TableCell(Mixin):
-    def __init__(self, items: Union[None, "ColumnSet", "Container", "Image", "Table"] = None,
-                 selectAction: None = None, style: Union[None, str] = None,
-                 verticalContentAlignment: Union[None, str] = None, bleed: Union[None, bool] = None,
-                 backgroundImage: Union[None, str] = None, minHeight: Union[None, str] = None,
-                 rtl: Union[None, bool] = None):
+    def __init__(self, items: Optional[Union["ColumnSet", "Container", "Image", "Table"]] = None,
+                 selectAction = None,
+                 style: Optional[str] = None,
+                 verticalContentAlignment: Optional[str] = None,
+                 bleed: Optional[bool] = None,
+                 backgroundImage: Optional[str] = None,
+                 minHeight: Optional[str] = None,
+                 rtl: Optional[bool] = None):
         pass
 # -------------------------Card Elements--------------------------- #
 
 
-class TextBlock(Mixin):
+class TextBlock(Mixin, CardElement):
     """Elemento de texto"""
-    def __init__(self, text: str = "", color: Union[None, str] = None, fontType: Union[None, str] = None,
-                 horizontalAlignment: Union[None, str] = None, isSubtle: Union[None, bool] = None,
-                 maxLines: Union[None, int] = None, size: Union[None, str] = None, weight: Union[None, str] = None,
-                 wrap: Union[None, bool] = None, style: Union[None, str] = None,
-                 fallback: Union[None, str, ColumnSet, "Container", "TextBlock"] = None, height: Union[None, str] = None,
-                 separator: Union[None, bool] = None, spacing: Union[None, str] = None, id: Union[None, str] = None,
-                 isVisible: Union[None, bool] = None):
+    def __init__(self,
+                 text: str = "",
+                 color: Optional[str] = None,
+                 fontType: Optional[str] = None,
+                 horizontalAlignment: Optional[str] = None,
+                 isSubtle: Optional[bool] = None,
+                 maxLines: Optional[int] = None,
+                 size: Optional[str] = None,
+                 weight: Optional[str] = None,
+                 wrap: Optional[bool] = None,
+                 style: Optional[str] = None,
+                 fallback: Optional[Union[str, ColumnSet, "Container", "TextBlock"]] = None,
+                 height: Optional[str] = None,
+                 separator: Optional[bool] = None,
+                 spacing: Optional[str] = None,
+                 id_: Optional[str] = None,
+                 isVisible: Optional[bool] = None):
+
         self.type = "TextBlock"
         self.text = text
-        super().create_fields(locals())
+        self.color = color
+        self.fontType = fontType
+        self.horizontalAlignment = horizontalAlignment
+        self.isSubtle = isSubtle
+        self.maxLines = maxLines
+        self.size = size
+        self.weight = weight
+        self.wrap = wrap
+        self.style = style
+        self.fallback = fallback
+        self.height = height
+        self.separator = separator
+        self.spacing = spacing
+        self.id = id_
+        self.isVisible = isVisible
 
-    def update_text(self, text):
-        self.text = text
+
+
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(text={self.text})"
+
+    def __str__(self):
+        return self.text
 
 
 class Image(Mixin):
-    def __init__(self, url: str, altText: Union[None, str] = None, backgroundColor: Union[None, str] = None,
-                 height: Union[None, str] = None, horizontalAlignment: Union[None, str] = None, selectAction: Union[None, str] = None):
+    def __init__(self,
+                 url: str,
+                 altText: Optional[str] = None,
+                 backgroundColor: Optional[str] = None,
+                 height: Optional[str] = None,
+                 horizontalAlignment: Optional[str] = None,
+                 selectAction: Optional[str] = None):
         self.type = "Image"
-        super().create_fields(locals())
--- adaptivecard/mixin.py
+++ adaptivecard/mixin.py
@@ -1,64 +1,37 @@
 import json
-# from typing import get_type_hints, Union
-# from typing_extensions import get_args, get_origin
+from typing import Any, get_type_hints
+from typeguard import check_type
 
 
 class Mixin:    # essa é uma classe do tipo mixin. Não tem funcionalidade própria, serve para fornecer funcionalidade a outras classes.
-    def create_fields(self, local_variables_dict) -> None:
-        variables = [parameter for parameter in self.__init__.__code__.co_varnames if parameter != "self"]
-        for variable in variables:
-            value = local_variables_dict[variable]
-            if value is not None:
-                self.__dict__[variable] = value
-
-    # def create_fields(self, local_variables_dict) -> None:
-    #     variables = get_type_hints(self.__init__)
-    #     for variable in variables:
-    #         value = local_variables_dict[variable]
-    #         expected_type = variables[variable]
-    #         if value is not None:
-    #             type_origin = get_origin(expected_type)
-    #             if type_origin is None:
-    #                 if type(value) != expected_type:
-    #                     msg = "'{variable}' attribute cannot be of type '{data_type}'"
-    #                     raise Exception(msg)
-    #             elif type_origin == list:
-    #                 if type(value) != list:
-    #                     msg = ""
-    #                     raise Exception(msg)
-    #                 else:
-    #                     expected_types_tuple = get_args(expected_type)
-    #                     type_origins_list = [x if get_origin(x) is None else get_origin(x) for x in expected_types_tuple]
-    #                     expected_types_inside_list = get_args(expected_types_tuple[type_origins_list.index(list)])
-    #                     if get_origin(expected_types_inside_list)[0] == Union:
-    #                         expected_types_inside_list = list(get_args(expected_types_inside_list[0]))
-    #                     for i in range(len(expected_types_inside_list)):
-    #                         if isinstance(expected_types_inside_list[i], str):
-    #                             expected_types_inside_list[i] == eval(expected_types_inside_list[i])
-    #                     for x in value:
-    #                         if type(x) not in expected_types_inside_list:
-    #                             msg = f"objects of type '{type(x)}' are not allowed in '{variable}' list"
-    #                             raise Exception(msg)
-    #             elif type_origin == Union:
-    #                 expected_types_tuple = get_args(expected_type)
-    #                 type_origins_list = [x if get_origin(x) is None else get_origin(x) for x in expected_types_tuple]
-    #                 if type(value) not in type_origins_list:
-    #                     msg = f"'{variable}' attribute cannot be of type {type(value)}"
-    #                     raise Exception(msg)
-    #                 elif type(value) == list:
-    #                     expected_types_inside_list = get_args(expected_types_tuple[type_origins_list.index(list)])
-    #                     if get_origin(expected_types_inside_list[0]) == Union:
-    #                         expected_types_inside_list = list(get_args(expected_types_inside_list[0]))
-    #                     for x in value:
-    #                         if type(x) not in expected_types_inside_list:
-    #                             msg = f"objects of type '{type(x)}' are not allowed in '{variable}' list"
-    #                             raise Exception(msg)
-
-    #             self.__dict__[variable] = value
 
     def to_dict(self):
-        dic = json.loads(json.dumps(self, default=lambda obj: obj.__dict__))  # isso é um hack muito ixpertinho
+        """
+        Returns a json/dictionary representative of the card element
+        """
+
+        def remove_null_values(dic: dict) -> dict:
+            return {key: dic[key] for key in dic if dic[key] is not None}
+
+        def get_json_dic(obj):
+            return {key for key in obj.__dict__ if key in obj.json_fields}
+
+        dic = json.loads(json.dumps(self, default=lambda obj: remove_null_values(get_json_dic(obj))))
         return dic
 
-    def set_version(self, version: str) -> None:
-        self.version = version
+    def get_local_vars(self) -> list:
+        variables = [var for var in vars(self).keys() if var != 'self']
+        return variables
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name in (type_hints := get_type_hints(self.__init__)):
+            check_type(__name, __value, type_hints[__name])
+        if __name == 'type' and hasattr(self, __name):
+            raise AttributeError(f"Can't set '{__name}' attribute")
+        super().__setattr__(__name, __value)
+
+    def __delattr__(self, __name: str) -> None:
+        if __name == 'json_fields':
+            raise AttributeError(f"Cannot delete '{__name}' attribute")
+        if __name in self.json_fields:
+            raise AttributeError('Cannot delete any card element attributes')
