e3f707dc7d33cde1e597e34b794a55ca250ce4b7
Luan Paz <luropa_paz@hotmail.com>
2023-06-29 18:32:31 -0300

    Altera versão

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.8'
+VERSION = '0.0.9'
 DESCRIPTION = 'Microsoft Adaptive Cards'
 LONG_DESCRIPTION = 'A package that helps you design adaptive cards in an object-oriented manner.'
 
