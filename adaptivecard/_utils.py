92a20ae9ed5ed49519996cb8e81b9062f1e610c2
Luan Rodrigues Paz <luropa_paz@hotmail.com>
2024-02-05 17:46:55 -0300

    Inclui Any type e trata tipos invalidos

--- adaptivecard/_utils.py
+++ adaptivecard/_utils.py
@@ -1,8 +1,15 @@
-from typing import get_origin, get_args, Union, Any
+from typing import get_origin, get_args, Union, Literal, Any, Iterable
 from types import NoneType, UnionType
 
 
 def check_type(arg_name: str, arg_value: Any, expected_type: tuple):
+    if expected_type is Any:
+        return
+    # se for Literal
+    if get_origin(expected_type) is Literal:
+        literals = get_args(expected_type)
+        if arg_value not in literals:
+            raise TypeError(f"argument '{arg_name}' must be equal to one of {tuple(literals)}, got {arg_value} instead")
     # se for None
     if expected_type is NoneType and arg_value is not None:
         raise TypeError(f"argument '{arg_name}' must an instance of {NoneType}")
@@ -17,10 +24,8 @@ def check_type(arg_name: str, arg_value: Any, expected_type: tuple):
                 continue
         else:
             raise TypeError(f"argument '{arg_name}' must be one of {tuple((tp.__name__ for tp in expected_type))}, got {type(arg_value).__name__} instead")        
-    # se for tipo parametrizado. Tipos contêiner só podem ter um parâmetro (com exceção da tupla)
-    elif len((type_subscripts := get_args(expected_type))) > 0:
-        if len(type_subscripts) > 1:
-            raise Exception("Invalid type hint")
+    # se for tipo contêiner parametrizado. Tipos contêiner só podem ter um parâmetro (com exceção da tupla)
+    elif len((type_subscripts := get_args(expected_type))) > 0 and isinstance(arg_value, Iterable):
         if not isinstance(arg_value, get_origin(expected_type)):
             raise TypeError(f"argument '{arg_name}' must be an instance of {get_origin(expected_type).__name__}, got {type(arg_value).__name__} instead")
         for inner_value in arg_value:
@@ -32,5 +37,9 @@ def check_type(arg_name: str, arg_value: Any, expected_type: tuple):
                 allowed_types = tuple([tp.__name__ for tp in allowed_types])
                 raise TypeError(f"argument '{arg_name}' can only contain instances of {allowed_types}")
     else:
+        try:
+            isinstance(arg_value, expected_type)
+        except TypeError:
+            raise TypeError("Invalid type")
         if not isinstance(arg_value, expected_type):
             raise TypeError(f"argument '{arg_name}' must be an instance of {expected_type.__name__}, got {type(arg_value).__name__} instead")
