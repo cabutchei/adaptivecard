f55d726117cdfaedb6c2eee3206f043ba7d291df
Luan Rodrigues Paz <luropa_paz@hotmail.com>
2024-02-05 19:34:48 -0300

    Reescreve método to_dict para evitar dependencia json e o problema de referencia circular

--- adaptivecard/_mixin.py
+++ adaptivecard/_mixin.py
@@ -1,40 +1,26 @@
 import json
-from typing import Any, get_type_hints, Sequence
-from typeguard import check_type
-from adaptivecard._base_types import Element
+from typing import Any, Iterable, get_type_hints
+from adaptivecard._utils import check_type
+from functools import partial
 
 
 class Mixin:
-
-    protected_attributes = frozenset(('json_fields', 'type'))
 
     def to_dict(self):
         """
         Returns a json/dictionary representative of the card element
         """
-
-        def get_json_dic(obj):
-            return {attr_name: attr_value for attr_name in obj.__slots__ if hasattr(obj, attr_name)
-                    and (attr_value := getattr(obj, attr_name)) is not None}
-
-        # possível problema de circular reference ao passar o mesmo objeto em dois lugares diferentes do card. Averiguar depois.
-        dic = json.loads(json.dumps(self, default=lambda obj: get_json_dic(obj)))
+        dic = {}
+        for attr_name, attr_value in zip(self.__slots__, map(partial(getattr, self), self.__slots__)):
+            if hasattr(attr_value, "__slots__"):
+                dic[attr_name] = attr_value.to_dict()
+            elif isinstance(attr_value, Iterable) and not isinstance(attr_value, str):
+                dic[attr_name] = [inner_value.to_dict() for inner_value in attr_value if hasattr(inner_value, "__slots__")]
+            elif attr_value is not None:
+                dic[attr_name] = attr_value
         return dic
-    
-    def is_sequence(self, value):
-        if isinstance(value, Sequence) and not isinstance(value, str):
-            return True
-        return False
 
     def __setattr__(self, __name: str, __value: Any) -> None:
         type_hints = get_type_hints(self.__init__)
-        if __name in type_hints:
-                check_type(__value, type_hints[__name])
-        if __name in self.protected_attributes and hasattr(self, __name):
-            raise AttributeError(f"Can't set '{__name}' attribute")
+        check_type(__name, __value, type_hints.get(__name, Any))
         super().__setattr__(__name, __value)
-
-    def __delattr__(self, __name: str) -> None:
-        if __name in self.protected_attributes or hasattr(self, 'json_fields') and __name in self.json_fields:
-            raise AttributeError(f"Cannot delete '{__name}' attribute")
-
