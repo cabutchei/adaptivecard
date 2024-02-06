bf75bb121804f99f9595b2d7d2cfb4fd830d407a
Luan Rodrigues Paz <luropa_paz@hotmail.com>
2024-02-06 19:27:30 -0300

    Move responsabilidade da padronizacao de atributos tipo items para o setattr de cada classe

--- adaptivecard/containers.py
+++ adaptivecard/containers.py
@@ -1,10 +1,10 @@
 from typing import Any, Optional, Union, Sequence, List, get_type_hints
 from typing_extensions import Literal
-from adaptivecard._base_types import Element, Action, Iselect_action
+from adaptivecard._base_types import Element, Action, ISelectAction, BaseColumnSet, BaseColumn
 from adaptivecard._mixin import Mixin
 from tabulate import tabulate
-from typeguard import check_type
 from adaptivecard.card_elements import TextBlock
+from adaptivecard._utils import check_type
 
 
 
@@ -60,41 +60,7 @@ class Container(Mixin, Element):
         return super().__setattr__(__name, __value)
 
 
-class ColumnSet(Mixin, Element):
-    """ColumnSet define um grupo de colunas"""
-    __slots__ = ('type', 'columns', 'style', 'bleed', 'min_height', 'horizontal_alignment', 'height',
-                                      'separator', 'spacing', 'id', 'is_visible')
-    def __init__(self, columns: Optional[Sequence["Column"]] = None,
-                 style: Optional[Literal["default", "emphasis", "good", "attention", "warning", "accent"]] = None,
-                 bleed: Optional[bool] = None,
-                 min_height: Optional[str] = None,   # pensar em algum type check para isso
-                 horizontal_alignment: Optional[Literal["left", "center", "right"]] = None,
-                 height: Optional[Literal["auto", "stretch"]] = None,
-                 separator: Optional[bool] = None,
-                 spacing: Optional[Literal["default", "none", "small", "medium", "large", "extraLarge", "padding"]] = None,
-                 id_: Optional[str] = None,
-                 is_visible: Optional[bool] = None):
-
-        if columns is None:
-            columns = []
-
-        self.type = 'ColumnSet'
-        self.columns = list(columns)
-        self.style = style
-        self.bleed = bleed
-        self.min_height = min_height
-        self.horizontal_alignment = horizontal_alignment
-        self.height = height
-        self.separator = separator
-        self.spacing = spacing
-        self.id = id_
-        self.is_visible = is_visible
-
-    def add_to_columns(self, column_element):
-        self.columns.append(column_element)
-
-
-class Column(Mixin, Element):
+class Column(Mixin, Element, BaseColumn):
     """O contâiner Column define um elemento de coluna, que é parte de um ColumnSet."""
     __slots__ = ('type', 'items', 'background_image', 'bleed', 'fallback', 'min_height', 'rtl', 'separator',
                                       'spacing', 'style', 'vertical_content_alignment', 'rtl', 'width', 'id', 'is_visible')
@@ -113,11 +79,8 @@ class Column(Mixin, Element):
                  id: Optional[str] = None,
                  is_visible: Optional[bool] = None):
 
-        if items is None:
-            items = []
-
         self.type = "Column"
-        self.items = list(items)
+        self.items: list = items
         self.background_image = background_image
         self.bleed = bleed
         self.fallback = fallback
@@ -131,35 +94,76 @@ class Column(Mixin, Element):
         self.id = id
         self.is_visible = is_visible
 
-    def add_to_items(self, card_element):
+    def append(self, card_element: Element):
         self.items.append(card_element)
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == "items":
+            items = __value
+            if items is None:
+                items = []
+            elif not isinstance(items, list):
+                items = list(items)
+            __value = items
+        return super().__setattr__(__name, __value)
+
+
+class ColumnSet(Mixin, Element, BaseColumnSet):
+    """ColumnSet define um grupo de colunas"""
+    __slots__ = ('type', 'columns', 'style', 'bleed', 'min_height', 'horizontal_alignment', 'height',
+                                      'separator', 'spacing', 'id', 'is_visible')
+    def __init__(self, columns: Optional[Sequence[Column]] = None,
+                 style: Optional[Literal["default", "emphasis", "good", "attention", "warning", "accent"]] = None,
+                 bleed: Optional[bool] = None,
+                 min_height: Optional[str] = None,   # pensar em algum type check para isso
+                 horizontal_alignment: Optional[Literal["left", "center", "right"]] = None,
+                 height: Optional[Literal["auto", "stretch"]] = None,
+                 separator: Optional[bool] = None,
+                 spacing: Optional[Literal["default", "none", "small", "medium", "large", "extraLarge", "padding"]] = None,
+                 id_: Optional[str] = None,
+                 is_visible: Optional[bool] = None):
+
+        self.type = 'ColumnSet'
+        self.columns = columns
+        self.style = style
+        self.bleed = bleed
+        self.min_height = min_height
+        self.horizontal_alignment = horizontal_alignment
+        self.height = height
+        self.separator = separator
+        self.spacing = spacing
+        self.id = id_
+        self.is_visible = is_visible
+
+    def append(self, column_element: Column):
+        # check_type() aceitar lista tbm?
+        self.columns.append(column_element)
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == "columns":
+            columns = __value
+            if columns is None:
+                columns = []
+            elif not isinstance(columns, list):
+                columns = list(columns)
+        return super().__setattr__(__name, __value)
+
 
 
 class TableCell(Mixin):
     __slots__ = ('type', 'items', 'select_action', 'style', 'vertical_alignment', 'bleed', 'background_image', 'min_height', 'rtl')
     def __init__(self,
                  items: Optional[Union[Any, Sequence[Any]]] = None,
-                 select_action: Optional[Iselect_action] = None,
+                 select_action: Optional[ISelectAction] = None,
                  style: Optional[Literal["default", "emphasis", "good", "attention", "warning", "accent"]] = None,
                  vertical_alignment: Optional[Literal["top", "center", "bottom"]] = None,
                  bleed: Optional[bool] = None,
                  background_image: Optional[str] = None,
                  min_height: Optional[str] = None,
                  rtl: Optional[bool] = None):
-      
-        if items is None:
-            items = []
-        elif isinstance(items, Element):
-            items = [items]
-        elif not self.is_sequence(items):
-            items = [TextBlock(text=str(items))]
-        else:
-            items = list(items)
-            for i, item in enumerate(items):
-                if not isinstance(item, Element):
-                    items[i] = TextBlock(text=str(item))
+
         self.type = "TableCell"
-        self.items = items
+        self.items: list = items
         self.select_action = select_action
         self.items = items
         self.style = style
@@ -169,14 +173,31 @@ class TableCell(Mixin):
         self.min_height = min_height
         self.rtl = rtl
 
-    def add_to_items(self, element: Element):
+    def append(self, element: Element):
         self.items.append(element)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(items={[str(item) for item in self.items]})"
+        return f"{self.__class__.__name__}(items={[item for item in self.items]})"
 
     def __str__(self) -> str:
         return "[" + ", ".join([str(item) for item in self.items]) + "]"
+
+    def __iter__(self):
+        ...
+    
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == "items":
+            items = __value
+            if items is None:
+                items = []
+            elif isinstance(items, Element):
+                items = [items]
+            elif isinstance(items, Sequence):
+                items = [item if isinstance(item, Element) else TextBlock(str(item)) for item in items]
+            else:
+                items = [TextBlock(str(items))]
+            __value = items
+        return super().__setattr__(__name, __value)
 
 
 class TableRow(Mixin, Sequence):
@@ -198,6 +219,8 @@ class TableRow(Mixin, Sequence):
         return self.__class__(self.cells + __value.cells)
     def __len__(self):
         return len(self.cells)
+    def __iter__(self):
+        ...
     def __repr__(self):
         return f"{self.__class__.__name__}({str(self.cells)})"
     def __str__(self):
@@ -205,95 +228,87 @@ class TableRow(Mixin, Sequence):
 
 
 class Table(Mixin, Element):
-    __slots__ = ('type', '_columns', 'rows', 'firstRowAsHeader', 'showGridLines', 'gridStyle',
+    __slots__ = ('type', 'columns', 'rows', 'first_row_as_header', 'show_grid_lines', 'grid_style',
                  'horizontal_cell_content_alignment', 'vertical_content_alignment', 'fallback', 'height',
                  'separator', 'spacing', 'id', 'is_visible')
     def __init__(self,
                  rows: Optional[Sequence[Sequence[Union[Element, TableCell]]]] = None,
-                 firstRowAsHeader: Optional[bool] = None,
-                 columns: Sequence[int] = [],
-                 showGridLines: Optional[bool] = None,
-                 gridStyle: Optional[Literal["default", "emphasis", "good", "attention", "warning", "accent"]] = None,
+                 first_row_as_header: Optional[bool] = None,
+                 columns: Sequence[int] = None,
+                 show_grid_lines: Optional[bool] = None,
+                 grid_style: Optional[Literal["default", "emphasis", "good", "attention", "warning", "accent"]] = None,
                  horizontal_cell_content_alignment: Optional[Literal["left", "center", "right"]] = None,
                  verticalCellContentAlignment: Optional[Literal["top", "center", "bottom"]] = None,
                  fallback: Optional[Element] = None,
                  height: Optional[Literal["auto", "stretch"]] = None,
                  separator: Optional[bool] = None,
                  spacing: Optional[Literal["default", "none", "small", "medium", "large", "extraLarge", "padding"]] = None,
-                 id_: Optional[str] = None,
+                 id: Optional[str] = None,
                  is_visible: Optional[bool] = None):
 
         self.type = "Table"
-
-        self.rows = rows
-        self._columns = list(columns)
-        self.firstRowAsHeader = firstRowAsHeader
-        self.showGridLines = showGridLines
-        self.gridStyle = gridStyle
+        self.rows: list = rows
+        self.columns = columns if columns is not None else []
+        self.first_row_as_header = first_row_as_header
+        self.show_grid_lines = show_grid_lines
+        self.grid_style = grid_style
         self.horizontal_cell_content_alignment = horizontal_cell_content_alignment
         self.verticalCellContentAlignment = verticalCellContentAlignment
         self.fallback = fallback
         self.height = height
         self.separator = separator
         self.spacing = spacing
-        self.id = id_
+        self.id = id
         self.is_visible = is_visible
-
-    @property
-    def columns(self):
-        if len(self.rows) > 0:
-            if len(self._columns) == 0:
-                return [{"width": 1} for _ in self.rows[0]]
-            else:
-                return [{"width": value} for value in self._columns]
-
-    @columns.setter
-    def columns(self, value):
-        check_type(value, get_type_hints(self.__init__)['columns'])
-        cols = list(value)
-        if len(self.rows) > 0 and len(cols) < len(self.rows[0]):
-            raise Exception("'length of columns must match the length of rows'")
-        self._columns = value
 
     def __getitem__(self, __i):
         return self.rows.__getitem__(__i)
     
-    def append_row(self, row: Sequence):
-        if not isinstance(row, Sequence) or isinstance(row, str):
-            raise Exception("'row' attribute must be a sequence of some kind")
-        row = TableRow(row)
+    def append(self, row: Sequence):
+        type_hints = get_type_hints(self.__init__)
+        check_type('row', row, type_hints.get('row', Any))
+        if not isinstance(row, TableRow):
+            row = TableRow(row)
         self.rows.append(row)
+    
+    # custom to_dict para lidar com o formato atípico do atributo columns dentro do json
+    def to_dict(self):
+        dic = super().to_dict()
+        if self.columns:
+            json_columns = [{"width": width} for width in self.columns]
+        elif self.rows:
+            json_columns = [{"width": 1} for _ in self.rows[0]]
+        else:
+            json_columns = dic["columns"]
+        dic["columns"] = json_columns
+        return dic
     
     def __len__(self):
         return len(self.rows)
     
     def __str__(self):
         rows = [["\n".join([str(item) for item in cell.items]) for cell in row.cells] for row in self.rows]
-        if self.firstRowAsHeader:
+        if self.first_row_as_header:
             headers = rows[0]
             rows = rows[1:]
         else:
             headers = []
         return tabulate(rows, headers=headers, tablefmt='grid')
 
+    def __iter__(self):
+        ...
+
     def __setattr__(self, __name: str, __value: Any) -> None:
-        if __name == 'rows':
+        if __name == "rows":
             rows = __value
             if rows is None:
                 rows = []
-            elif not self.is_sequence(rows):
-                raise TypeError("'rows' attribute must be a sequence of some kind")
-            elif len(rows) > 0 and not all(self.is_sequence(row) for row in rows):
-                raise TypeError("'rows' attribute must be a sequence of sequences")
-            else:
-                for i, row in enumerate(rows):
-                    for j, cell in enumerate(row):
-                        if not isinstance(cell, TableCell):
-                            row[j] = TableCell(cell)
+            for i, row in enumerate(rows):
+                for j, cell in enumerate(row):
+                    if not isinstance(cell, TableCell):
+                        row[j] = TableCell(cell)
+                if not isinstance(row, TableRow):
                     rows[i] = TableRow(cells=row)
-
-            if len(rows) > 1 and not all([len(rows[i]) == len(rows[i-1]) for i, _ in enumerate(rows[1:])]):
-                raise Exception("Length mismatch, all rows must have the same length")
             __value = rows
         return super().__setattr__(__name, __value)
 
@@ -310,7 +325,7 @@ class ActionSet(Mixin, Element):
                  is_visible: Optional[bool] = None
                  ) -> None:
         self.type = "ActionSet"
-        self.actions = actions
+        self.actions: list = actions
         self.fallback = fallback
         self.height = height
         self.separator = separator
@@ -318,12 +333,16 @@ class ActionSet(Mixin, Element):
         self.id = id
         self.is_visible = is_visible
 
-    def append_action(self, action: Action):
+    def append(self, action: Action) -> None:
         self.actions.append(action)
 
     def __setattr__(self, __name: str, __value: Any) -> None:
-        if __value is None:
-            __value = []
-        elif isinstance(__value, Element):
-            __value = [__value]
-        return super().__setattr__(__name, __value)
+        if __name == "actions":
+            actions = __value
+            if actions is None:
+                actions = []
+            elif isinstance(actions, Action):
+                actions = [actions]
+            elif not isinstance(actions, list):
+                actions = list(actions)
+            return super().__setattr__(__name, __value)
