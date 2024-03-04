41d56b4e80ece1b342155aae5ad065fddf6238a6
Luan Rodrigues Paz <luropa_paz@hotmail.com>
2024-03-04 02:25:29 -0300

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.9.0a0'
+VERSION = '0.2.0'
 DESCRIPTION = 'Microsoft Adaptive Cards'
 LONG_DESCRIPTION = 'A package that helps you design adaptive cards in an object-oriented manner.'
 
