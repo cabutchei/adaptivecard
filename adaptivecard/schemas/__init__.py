bf54873fcaf76ee7b8c292d093cce86820fa06c5
Luan Rodrigues Paz <luropa_paz@hotmail.com>
2024-09-20 19:34:31 -0300

    fix path

--- adaptivecard/schemas/__init__.py
+++ adaptivecard/schemas/__init__.py
@@ -2,5 +2,5 @@ import json
 from pathlib import Path
 
 
-with open(Path(".") / "adaptivecard" / "schemas" / "schema.json") as f:
+with open(Path(".") / "schemas" / "schema.json") as f:
     schema = json.load(f)
