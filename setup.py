5a5aed88376d84548a9bf09f80c2ab1c38253836
cabutchei <luropa_paz@hotmail.com>
2024-10-10 19:01:55 -0300

    0.3.0a11

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.3.0a10'
+VERSION = '0.3.0a11'
 DESCRIPTION = 'Microsoft Adaptive Cards'
 LONG_DESCRIPTION = 'A package that helps you design adaptive cards in an object-oriented manner.'
 
